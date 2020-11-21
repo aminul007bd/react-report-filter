1af0a570be0e5d80a59ecc1aef868cf77226afe5
aminul007bd <gem007bd@gmail.com>
2020-11-21 14:37:34 +0100

    add react-bootstrap with icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css'
-
+import { Container, Card, Button } from 'react-bootstrap'
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="App">
-      <h1>Report app</h1>
+      <Container fluid>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
   )
 }
