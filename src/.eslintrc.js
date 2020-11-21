c6537dd0c5f2df948c156bac60ba4a96caa1e1f3
aminul007bd <gem007bd@gmail.com>
2020-11-21 14:11:14 +0100

    add ts-lint with prettierc config

---
+++ src/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    extends: [
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:jest/recommended',
+      'prettier',
+      'prettier/react',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+    },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    rules: {
+      'linebreak-style': 'off',
+          // suppress errors for missing 'import React' in files
+      "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }], //should add ".ts" if typescript project
+      "prettier/prettier": ["error", {
+        "endOfLine":"auto"
+      }]
+    },
+  };
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import './App.css'
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="App">
       <h1>Report app</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
