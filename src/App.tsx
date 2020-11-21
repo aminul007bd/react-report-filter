8d175641dfae6a77463bbe573d3e164f437a42fa
aminul007bd <gem007bd@gmail.com>
2020-11-21 16:05:25 +0100

    add json server with report data and use axios to fetch it with react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,28 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
-import { Container, Card, Button } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [repos, setRepos] = useState<any[]>([])
+  // Local environment url for fetch mock api
+  const fetchURL = 'http://localhost:5000'
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`${fetchURL}/report`)
+      setRepos(response.data)
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className="App">
       <Container fluid>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        {repos.map((repo) => (
+          <div key={repo?.uuid}>{repo.body.bankName}</div>
+        ))}
       </Container>
     </div>
   )
